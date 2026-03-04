1118b52e7ae2c0068894876834318126a7330509
AanYadav <aayadav@ucsc.edu>
2026-03-03 18:25:44 -0800

    Refactor module and implement hex7seg logic
    Refactored module structure and added hex7seg functionality.

--- src/project.v
+++ src/project.v
@@ -1,27 +1,24 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Aan Yadav
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module hex7seg(
+    input  logic d3,d2,d1,d0,
+    output logic A,B,C,D,E,F,G
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-
+// TODO
+assign A = !((~d3&~d2&~d1&d0)|(~d3&d2&~d1&~d0)|(d3&~d2&d1&d0)|(d3&d2&~d1&d0));
+assign B = !((~d3&d2&~d1&d0)|(~d3&d2&d1&~d0)|(d3&~d2&d1&d0)|(d3&d2&~d1&~d0)
+            |(d3&d2&d1&~d0)|(d3&d2&d1&d0));
+assign C = !((~d3&~d2&d1&~d0)|(d3&d2&~d1&~d0)|(d3&d2&d1&~d0)|(d3&d2&d1&d0));
+assign D = !((~d3&~d2&~d1&d0)|(~d3&d2&~d1&~d0)|(~d3&d2&d1&d0)
+            |(d3&!d2&d1&!d0)|(d3&d2&d1&d0));
+assign E = !((~d3&~d2&~d1&d0)|(~d3&~d2&d1&d0)|(~d3&d2&~d1&~d0)|(~d3&d2&~d1&d0)
+            |(~d3&d2&d1&d0)|(d3&~d2&~d1&d0));
+assign F = !((~d3&~d2&~d1&d0)|(~d3&~d2&d1&~d0)|(~d3&~d2&d1&d0)|(~d3&d2&d1&d0)|(d3&d2&~d1&d0));
+assign G = !((~d3&~d2&~d1&~d0)|(~d3&~d2&~d1&d0)|(~d3&d2&d1&d0)|(d3&d2&~d1&~d0));
 endmodule
